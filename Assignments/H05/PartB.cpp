60016c0fb36c7c53d8f91835ca56244239396e93
PakaCat97 <cmunoz9755@gmail.com>
2025-05-01 15:41:04 -0500

    stuff

---
+++ Assignments/H05/PartB.cpp
@@ -0,0 +1,22 @@
+// Short code demonstrating the concept of abstraction by moddeling a banking system.
+
+#include <iostream>
+
+using namespace std;
+
+// Abstract Class
+class BankAccount
+{
+
+};
+
+class SavingsAccount
+{
+
+
+};
+
+int main()
+{
+
+}
